0c149cc6e5bd8003e9ad8a6dd85ed3415b2e17db
Abdou Khadre Alwaly Ndiaye <abdouyrepf@gmail.com>
2026-04-11 04:17:55 -1200

    style: correction hero et ajout menu burger

--- src/data/models.js
+++ src/data/models.js
@@ -1,38 +1,60 @@
 export const MODELS = [
   {
     id: 1,
-    title: "Robe de Mariée Royale",
+    title: "Robe de Mariée Bustier",
     category: "Mariage",
     image: "/rm1.jpeg",
-    description: "Soie sauvage et perles de culture."
+    description: "Un bustier cintré et travaillé associé à une jupe volumineuse ornée de motifs scintillants pour un éclat féerique."
   },
   {
     id: 2,
-    title: "Robe Traditionnelle senegalaise",
+    title: "Robe Brodée",
     category: "Traditionnel",
     image: "/t1.jpeg",
-    description: "Robe ."
+    description: "Robe longue asymétrique noire, ornée de délicates broderies florales orange et or pour une élégance intemporelle."
   },
   {
     id: 3,
-    title: "Ensemble Homme",
+    title: "Ensemble Homme Noir",
     category: "Prêt-à-porter",
-    image: "/pp1.jpeg",
-    description: "Coupe ajustée pour un look professionnel."
+    image: "/p1.jpeg",
+    description: "Tunique à col lacé et pantalon assorti, pour un style traditionnel revisité."
   },
   {
     id: 4,
-    title: "Robe evasee",
+    title: "Robe Passion Éclatante",
     category: "Soirée / Gala",
-    image: "/re1.jpeg",
-    description: "Coupe ajustée pour un look elegent."
+    image: "/m1.jpeg",
+    description: "Une robe de soirée rouge intense aux épaules dénudées, sublimée par une dentelle fine et des détails scintillants pour une allure captivante."
   },
   {
     id: 5,
     title: "Chemise de femme",
     category: "Traditionnel",
-    image: "/c1.jpeg",
+    image: "/t2.jpeg",
     description: "Chemise ajustée pour un look magnifique."
   },
+  {
+    id: 6,
+    title: "Ensemble Kimono Floral",
+    category: "Prêt-à-porter",
+    image: "/p2.jpeg",
+    description: "Un mélange audacieux de motifs floraux et de coupes fluides pour un style sophistiqué au quotidien."
+  },
+  {
+    id: 7,
+    title: "Abaya Signature",
+    category: "Prêt-à-porter",
+    image: "/p3.jpeg",
+    description: "Une pièce épurée alliant pudeur et modernité, parfaite pour les grandes occasions."
+  },
+  {
+    id: 8,
+    title: "Grand boubou luxe",
+    category: "Prêt-à-porter",
+    image: "/p4.jpeg",
+    description: "Finitions brodées à la main sur un tissu fluide pour une allure royale et distinguée."
+  },
+ 
 
 ];
